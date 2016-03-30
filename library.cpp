9299e16e55a22abe1c333218053f68eaaa994ff3
Blade Frisch <blade.frisch@gmail.com>
2016-03-30 08:27:52 -0400

    Added the printMenu function to library.cpp

--- library.cpp
+++ library.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
 #include <iomanip>
 #include <fstream>
+#include <cstdlib>
+#include "Book.h"
 using namespace std;
 
+//Function Prototypes
+void printMenu();
+
 int main(int argc, char** argv) {
+
+    printMenu();
+
+    return EXIT_SUCCESS;
+}
+
+void printMenu() {
+    cout << "##### Welcome to your library! #####" << endl;
+    cout << "     Please select your option:     " << endl;
+    cout << setw(16) << right << "1"               << "    "
+         << setw(16) << left  << "Show All Books"  << endl;
+    cout << setw(16) << right << "2"               << "    "
+         << setw(16) << left  << "Search Books"    << endl;
+    cout << setw(16) << right << "3"               << "    "
+         << setw(16) << left  << "Add Book"    << endl;
+    cout << setw(16) << right << "4"               << "    "
+         << setw(16) << left  << "Show All Tags"    << endl;
 
 }
