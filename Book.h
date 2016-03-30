b95ac6c58ad6723363662b0895097f42a03030c5
Blade Frisch <blade.frisch@gmail.com>
2016-03-30 08:04:48 -0400

    Added private data members and getters in Book.h

--- Book.h
+++ Book.h
@@ -1,7 +1,19 @@
 #ifndef BOOK_H
 #define BOOK_H
+#include <vector>
+#include <string>
 
 class Book {
+
+    private:
+        std::string              title;
+        std::string              author;
+        std::vector<std::string> tags;
+    public:
+        //Setters and Getters
+        std::string              getTitle()  {return title;}
+        std::string              getAuthor() {return author;}
+        std::vector<std::string> getTags()   {return tags;}
 
 };
 #endif
