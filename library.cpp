3e94649c36388018ade7a98e5954cf2cccdcd5b5
Blade Frisch <blade.frisch@gmail.com>
2016-03-30 08:37:37 -0400

    Added the getCmd function to library.cpp

--- library.cpp
+++ library.cpp
@@ -1,16 +1,22 @@
 #include <iostream>
 #include <iomanip>
 #include <fstream>
+#include <list>
 #include <cstdlib>
 #include "Book.h"
 using namespace std;
 
 //Function Prototypes
-void printMenu();
+void printMenu ();
+int  getCmd    ();
 
 int main(int argc, char** argv) {
 
+    int        cmd;
+    list<Book> library;
+
     printMenu();
+    cmd = getCmd();
 
     return EXIT_SUCCESS;
 }
@@ -27,4 +33,20 @@ void printMenu() {
     cout << setw(16) << right << "4"               << "    "
          << setw(16) << left  << "Show All Tags"    << endl;
 
+}
+
+int getCmd() {
+    int cmd;
+
+    while(true) {
+        cout << "\n\nPlease select an option number: ";
+        cin  >> cmd;
+
+        if(cmd >= 1 && cmd <= 4)
+            break;
+
+        cout << "Invalid choice! Please select between 1 and 4" << endl;
+    }
+
+    return cmd;
 }
