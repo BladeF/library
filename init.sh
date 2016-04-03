91303b3d6280298e2b8814fffb840fa4fb05ab95
Blade Frisch <blade.frisch@gmail.com>
2016-04-03 18:12:48 -0400

    Changed permissions in init.sh for executable

--- init.sh
+++ init.sh
@@ -8,12 +8,12 @@ else
     echo "Data directory creation failed"
 fi
 
-chmod 755 src/* lib/*
+chmod 755 src/*
 chmod 755 data  src scripts
 
 g++ -o library src/library.cpp src/Book.cpp
 
-chmod 444 ./library
+chmod 111 ./library
 
 mv init.sh scripts
 chmod 555 *.sh scripts/*.sh
