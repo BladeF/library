674c46f1e795b5f116785ab24083d5d875ae9690
Blade Frisch <blade.frisch@gmail.com>
2016-04-03 18:15:22 -0400

    Updated quit.sh flow control

--- scripts/quit.sh
+++ scripts/quit.sh
@@ -2,8 +2,12 @@
 
 cd data
 
-rm bookData.txt
-rm tagData.txt
+if [ -f bookData.txt ]; then
+    rm bookData.txt
+fi
+if [ -f tagData.txt ]; then
+    rm tagData.txt
+fi
 
 cat bookOutput.txt | tee bookData.txt
 cat tagOutput.txt | tee tagData.txt
