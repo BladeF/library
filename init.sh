c1479f3829a30579d85269fa69bc707e1eea984b
Blade Frisch <blade.frisch@gmail.com>
2016-04-04 11:10:35 -0400

    Updated the init.sh script to move executable to exe dir

--- init.sh
+++ init.sh
@@ -8,12 +8,21 @@ else
     echo "Data directory creation failed"
 fi
 
+mkdir exe
+if [ -d exe ]
+then
+    echo "Exe directory creation successful"
+else
+    echo "Exe directory creation failed"
+fi
+
 chmod 755 src/*
-chmod 755 data  src scripts
+chmod 755 data src scripts
 
 g++ -o library src/library.cpp src/Book.cpp
 
 chmod 111 ./library
+mv ./library exe
 
 mv init.sh scripts
 chmod 555 *.sh scripts/*.sh
