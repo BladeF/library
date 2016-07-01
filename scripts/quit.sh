6da9b55d4b3e7fc28f7a96641bd2fd1368ca92af
Blade Frisch <blade.frisch@gmail.com>
2016-07-01 09:58:18 -0400

    Updated the quit script to handle no output from program

--- scripts/quit.sh
+++ scripts/quit.sh
@@ -14,7 +14,17 @@ fi
 # for the next run of the program
 if [ -f bookOutput.txt ]; then
     mv bookOutput.txt bookData.txt
+
+    # Removes file if it's empty
+    if [ ! -s bookData.txt ]; then
+        rm bookData
+    fi
 fi
 if [ -f tagOutput.txt ]; then
     mv tagOutput.txt tagData.txt
+
+    # Removes file if it's empty
+    if [ ! -s tagData.txt ]; then
+        rm tagData
+    fi
 fi
